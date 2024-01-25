4b08edff7ec768bb0711cab19e1c4e648a971495
Feichuan <Feichuan05@gamil.com>
2024-01-25 17:29:24 +0800

    绘制漫步图

---
+++ Data_visualization/practice1.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt 
+
+x_values = range(1,5)
+x2_values = range(1,500)
+y_values = [x**3 for x in x_values]
+y2_values = [x**3 for x in x2_values]
+
+fig,ax =fig,zx =  plt.subplots()
+zx.scatter(x_values,y_values)
+ax.scatter(x2_values,y2_values,c=y2_values,cmap=plt.cm.Blues,)
+
+
+plt.show()
